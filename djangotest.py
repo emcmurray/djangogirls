3f0558a99f4002174c191ea39d6e4dddf53cf53a
Elizabeth McMurray <el.mcmurray@gmail.com>
2015-07-11 11:14:03 +1000

    My Django Girls app, first commit

---
+++ djangotest.py
@@ -0,0 +1,11 @@
+print('Hello, Django girls!')
+if 3 > 2:
+    print('It works!')
+
+name = 'Sonja'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Sonja':
+    print('Hey Sonja!')
+else:
+    print('Hey anonymous!')
